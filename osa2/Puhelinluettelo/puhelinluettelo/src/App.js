52c104772f3e2c97f16ad461dcad928428831d81
Valtteri Valtonen <valtonenvaltteri@gmail.com>
2019-09-09 11:35:44 +0300

    Osa 2 Puhelinluettelo 2 ja 3

--- osa2/Puhelinluettelo/puhelinluettelo/src/App.js
+++ osa2/Puhelinluettelo/puhelinluettelo/src/App.js
@@ -6,20 +6,26 @@ const App = () => {
   ]) 
   const [ newName, setNewName ] = useState('')
 
+  const [newNum, setNewNum] = useState('')
+
 //Tapahtumankäsittelijäetodi joka lisää henkilölistaan uuden ihmisen
   const addNewPerson = (event) => {
     
     event.preventDefault()
 
-    if(newName !== ''){
-
-    const newObj = {name: newName}
+    if(!persons.map(person => person.name).includes(newName)){
+    const newObj ={
+     name: newName,
+     number: newNum
+    }
+   
     setPersons(persons.concat(newObj))
     setNewName('')
+    setNewNum('')
 
     }else{
 
-      console.log('No name given.')
+      alert(`${newName} is already added to phonebook`)
 
     }
       
@@ -28,24 +34,27 @@ const App = () => {
 /*Metodi joka hoitaa newname-tilan päivityksen kenttään
   kirjoitettaessa.
 */
-  const handleWrite = (event) => {
+  const handleNameWrite = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberWrite = (event) => {
+    setNewNum(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addNewPerson}>
-        <div>
-          name: <input value={newName} onChange = {handleWrite}/>
-        </div>
+        <div> name: <input value={newName} onChange = {handleNameWrite}/></div>
+        <div> number: <input value = {newNum} onChange = {handleNumberWrite}/> </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-       {persons.map(person => <li key={person.name}>{person.name}</li>)}
+       {persons.map(person => <li key={person.name}>{person.name}  {person.number}</li>)}
       </ul>
     </div>
   )
