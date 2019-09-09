991fecf25c66ff254a7e7a8a28a888d15c32be8e
Valtteri Valtonen <valtonenvaltteri@gmail.com>
2019-09-09 20:42:45 +0300

    Osa 2 Puhelinluettelo 4

--- osa2/Puhelinluettelo/puhelinluettelo/src/App.js
+++ osa2/Puhelinluettelo/puhelinluettelo/src/App.js
@@ -2,11 +2,18 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
+
+
   const [ newName, setNewName ] = useState('')
 
   const [newNum, setNewNum] = useState('')
+
+  const [filterLetters, setFilterLetters] = useState('')
 
 //Tapahtumankäsittelijäetodi joka lisää henkilölistaan uuden ihmisen
   const addNewPerson = (event) => {
@@ -42,9 +49,23 @@ const App = () => {
     setNewNum(event.target.value)
   }
 
+  const handleFilter = (event) => {
+    setFilterLetters(event.target.value)
+
+    console.log(filterLetters)
+
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+
+
+      <form>
+        <div>Filter shown with <input value = {filterLetters} onChange = {handleFilter}/></div>
+
+      </form>
+
       <form onSubmit={addNewPerson}>
         <div> name: <input value={newName} onChange = {handleNameWrite}/></div>
         <div> number: <input value = {newNum} onChange = {handleNumberWrite}/> </div>
@@ -53,11 +74,35 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-       {persons.map(person => <li key={person.name}>{person.name}  {person.number}</li>)}
-      </ul>
+      <Listofpeople list = {persons.filter(person => person.name.slice(0, filterLetters.length) === filterLetters)} />
+       
+      
     </div>
   )
+
+}
+
+const Person = ({name, number}) => {
+
+return( 
+<div>
+<p>{name}  {number}</p>
+</div>
+)
+
+}
+
+
+//Komponentti joka renderöi ihmislistan. 
+//Huolehtii filtteröinnistä.
+const Listofpeople = ({list}) => {
+
+return(
+
+  <div>{list.map(dataItem => <Person key = {dataItem.name} name = {dataItem.name} number = {dataItem.number}/>)}</div>
+
+)
+
 
 }
 
